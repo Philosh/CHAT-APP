1f4cb53207040a94c5a0bc26f654b0f1351c45d8
SaugatPersonal <SaugatPersonal@Saugats-MacBook-Pro.local>
2023-10-13 16:37:01 +0300

    MongoDB configs introduced

--- server/server.js
+++ server/server.js
@@ -1,24 +1,19 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const { chats } = require("./data/data");
+const connectDB = require("./config/db");
+const colors = require("colors");
 
 const app = express();
 dotenv.config();
+connectDB();
 
 app.get("/", (req, res) => {
   res.send("API is running successfully");
 });
 
-app.get("/api/chat/", (req, res) => {
-  res.send(chats);
-});
-
-app.get("/api/chat/:id", (req, res) => {
-  //console.log(req.params.id);
-  const singleChat = chats.find((c) => c._id === req.params.id);
-  res.send(singleChat);
-});
+app.use("/api/user", userRoutes);
 
 const PORT = process.env.PORT || 3001;
 
-app.listen(PORT, console.log(`Server is running on PORT ${PORT}`));
+app.listen(PORT, console.log(`Server is running on PORT ${PORT}`.yellow.bold));
