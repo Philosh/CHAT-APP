4c77c0dfb4fd2b9df57621f7dd601855642dbde0
SaugatPersonal <SaugatPersonal@Saugats-MacBook-Pro-2.local>
2023-11-07 00:36:58 +0200

    Created chats endpoints

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,2 +1,17 @@
 const express = require("express");
 const router = express.Router();
+const {
+  registerUser,
+  authUser,
+  allUsers,
+} = require("../controllers/userControllers");
+
+const { protect } = require("../middleware/authMiddleware");
+
+router.post("/", registerUser);
+
+router.get("/", protect, allUsers);
+
+router.post("/login", authUser);
+
+module.exports = router;
